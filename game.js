aa2e8f596755e318946a8fb02dd233cde55b7430
Karolina Redden <dubajkaro@gmail.com>
2023-11-13 14:25:18 -0400

    add basic table

--- game.js
+++ game.js
@@ -1,10 +1,23 @@
 // const cors = require('cors');
 
+
+
+
 async function fetchGameAPI() {
   const response = await fetch('https://prog2700.onrender.com/threeinarow/sample');
   // waits until the request completes...
-  console.log(response.rows);
   const gameDataAPI = await response.json();
   console.log(gameDataAPI);
+};
+
+document.querySelector('#theGame').innerHTML = createGameTable();
+
+function createGameTable() {
+  let gameHTML = "<table>";
+  gameHTML += "</table>";
+  return gameHTML;
 }
+
+
+createGameTable();
 fetchGameAPI();
