bdd83723a9943d2439e0f7c36928b4a7caee3eb0
Karolina Redden <dubajkaro@gmail.com>
2023-11-13 13:08:03 -0400

    jsonify the response

--- game.js
+++ game.js
@@ -1,6 +1,10 @@
+// const cors = require('cors');
+
 async function fetchGameAPI() {
   const response = await fetch('https://prog2700.onrender.com/threeinarow/sample');
   // waits until the request completes...
-  console.log(response);
+  console.log(response.rows);
+  const gameDataAPI = await response.json();
+  console.log(gameDataAPI);
 }
 fetchGameAPI();
