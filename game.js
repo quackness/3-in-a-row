da920d6bfe24effd76d6e3f8fac1b89a79d5fed5
Karolina Redden <dubajkaro@gmail.com>
2023-11-13 15:25:02 -0400

    save work in progress

--- game.js
+++ game.js
@@ -1,31 +1,46 @@
 
+
 async function fetchGameAPI() {
   let response = await fetch('https://prog2700.onrender.com/threeinarow/sample');
   // waits until the request completes...
   let gameDataAPI = await response.json();
   gameDataAPI = gameDataAPI.rows;
   console.log(gameDataAPI);
-  let rows = "";
-  for (let i = 0; i < gameDataAPI.length; i++) {
-    rows += "<tr>";
-    console.log(">>", gameDataAPI[i])
-    for (let j = 0; j < gameDataAPI[i].length; j++) {
-      console.log("inner array", gameDataAPI[i][j]);
+  document.querySelector('#theGame').innerHTML = createTable();
+  function createTable() {
+    let table = "<table>";
+    let rows = "";
+    for (let i = 0; i < gameDataAPI.length; i++) {
+      rows += "<tr>";
+      console.log(">>", gameDataAPI[i])
+      let tableData = ""
+      for (let j = 0; j < gameDataAPI[i].length; j++) {
+        //gameDataAPI[i][j] is an object, must loop through it 
+        // tableData += "<td>" + gameDataAPI[i][j] + "</td>";
+        // console.log("table data", gameDataAPI[i][j]);
+        tableData += "<td> Data </td>";
+        console.log("table data", tableData);
+      }
+      rows += tableData;
+      rows += "</tr>";
+      console.log("rows", rows)
     }
-    rows += "</tr>";
-    console.log("rows", rows)
+    table += rows;
+    table += "</table>";
+    console.log(table);
   }
+
 };
 
 
-document.querySelector('#theGame').innerHTML = createGameTable();
 
-function createGameTable() {
-  let gameHTML = "<table>";
-  gameHTML += "</table>";
-  return gameHTML;
-}
+createTable();
+// function createGameTable() {
+//   let gameHTML = "<table>";
+//   gameHTML += "</table>";
+//   return gameHTML;
+// }
 
 
-createGameTable();
+
 fetchGameAPI();
