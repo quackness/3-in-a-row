3e0bc291f3db96de1912dd2ba186dc8e1784116e
Karolina Redden <dubajkaro@gmail.com>
2023-11-15 21:44:23 -0400

    change tile colour on click

--- game.js
+++ game.js
@@ -14,32 +14,43 @@ async function fetchGameAPI() {
     let rows = "";
     for (let i = 0; i < gameDataAPI.length; i++) {
       rows += "<tr>";
-      console.log(">>", gameDataAPI[i])
+      // console.log(">>", gameDataAPI[i])
       let tableData = ""
       for (let j = 0; j < gameDataAPI[i].length; j++) {
-        // singleData = gameDataAPI[i][j];
         tableData += `<td id='row${i}col${j}' class='state${gameDataAPI[i][j].currentState}'></td>`;
-        console.log("table data", tableData);
+        // console.log("table data", tableData);
       }
       rows += tableData;
       rows += "</tr>";
-      console.log("rows", rows)
+      // console.log("rows", rows)
     }
     table += rows;
     return table += "</table>";
-    console.log(table);
+    // console.log(table);
   }
 
-  function displayDate(event) {
-    console.log(event);
-    // in target find IdleDeadline, parse it to extract the row and column
+  function changeTileColour(event) {
+    console.log("event", event);
+    console.log("event", event.target.id);
+    let classtoBeChanged = document.getElementById(event.target.id)
+    console.log("classtoBeChanged", classtoBeChanged.className);
+    if (classtoBeChanged.className === "state0") {
+      classtoBeChanged.className = "state1"
+    } else if (classtoBeChanged.className === "state1") {
+      classtoBeChanged.className = "state2"
+    } else {
+      classtoBeChanged.className = "state0"
+    }
   }
+
+  // in target find IdleDeadline, parse it to extract the row and column
+
 
   let td = document.querySelectorAll('td')
   for (let i = 0; i < td.length; i++) {
-    td[i].addEventListener("click", displayDate);
+    td[i].addEventListener("click", changeTileColour);
     //event.target.id
-    console.log(event.target.id)
+    // console.log(event.target.id)
   }
 
 
